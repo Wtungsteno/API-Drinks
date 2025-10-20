018680f18999091c5eab68cfcaaa1ba1dc67a29c
Giacomo Lesa <giacomo.lesa@itsincom.it>
2025-10-20 11:37:15 +0200

    DrinksQuery Added

---
+++ B_BL.GraphQL/DrinksQuery.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPILoGiud;
+using WebAPILoGiud.DTO;
+
+namespace B_BL.GraphQL
+{
+    public class DrinksQuery
+    {
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<DrinkDto> GetDrinks(DrinkDbContext context)
+        {
+            return context
+                .Drinks
+                .AsNoTracking()
+                .Select(d => new DrinkDto
+                {
+                    Id = d.Id,
+                    Name = d.Name,
+                    Size = d.Size,
+                    Fizz = d.Fizz
+                });
+        }
+    }
+}
